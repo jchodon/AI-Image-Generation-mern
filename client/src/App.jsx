973cad9b684004e5352c3b41eee098c2595f446e
jchodon <jigcho27@gmail.com>
2023-12-18 17:18:25 -0500

    tailwind config

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
-
+import { BrowserRouter, Form, Link, Route, Routes } from "react-router-dom";
+import { logo } from "./assets/index.js";
+import { Home, CreatePost } from "./pages/index.js";
 function App() {
-  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+        <Link to="/">
+          <img src={logo} alt="logo" className="w-28 object-contain" />
+        </Link>
+      </header>
+    </BrowserRouter>
+  );
 }
 
 export default App;
